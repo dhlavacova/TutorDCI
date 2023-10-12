3eee462b4076444fdca0b0b16e1d78fd5be31f07
Dana <dana@hlavacova.de>
2023-10-12 08:48:44 +0200

    odesilam data

--- client/src/pages/TutoringBookingForm.jsx
+++ client/src/pages/TutoringBookingForm.jsx
@@ -1,158 +1,165 @@
-import React, { useEffect, useState } from "react";
-import { useInfoTutor } from "../context/infotutorContext";
-import { Button, Card, Label, Input } from "../components/ui";
-import { FaCalendar, FaClock, FaHourglass } from "react-icons/fa";
+import React, {useEffect, useState} from "react";
+import {useInfoTutor} from "../context/infotutorContext";
+import {Button, Card, Input, Label} from "../components/ui";
 
 export function TutoringBookingForm() {
-  const { allInfoTutors, getAllInfoTutors } = useInfoTutor();
-  const [selectedTutor, setSelectedTutor] = useState(null);
-  const [selectedDay, setSelectedDay] = useState("");
-  const [selectedDayNumber, setSelectedDayNumber] = useState(undefined);
-  const [selectedTime, setSelectedTime] = useState("");
-  const [theme, setTheme] = useState("");
-  const [date, setDate] = useState("");
-const [timeout,setTimeout]=useState(undefined)
-  useEffect(() => {
-    getAllInfoTutors()
-      .then(() => {
-        console.log(allInfoTutors); // Depuración
-      })
-      .catch((error) => {
-        console.error("Error", error);
-      });
-  }, []);
+    const {allInfoTutors, getAllInfoTutors} = useInfoTutor();
+    const [selectedTutor, setSelectedTutor] = useState(null);
+    const [selectedDay, setSelectedDay] = useState("");
+    const [selectedDayNumber, setSelectedDayNumber] = useState(undefined);
+    const [selectedTime, setSelectedTime] = useState("");
+    const [theme, setTheme] = useState("");
+    const [date, setDate] = useState("");
+    const [timeout, setTimeout] = useState({})
+    useEffect(() => {
+        getAllInfoTutors()
+            .then(() => {
+                console.log(allInfoTutors); // Depuración
+            })
+            .catch((error) => {
+                console.error("Error", error);
+            });
+    }, []);
 
-console.log({allInfoTutors});
-  const handleTutorChange = (event) => {
-    const tutorId = event.target.value;
-    const selected = allInfoTutors.tutors.find((tutor) => tutor._id === tutorId);
-    setSelectedTutor(selected);
-    setSelectedDay("");
-    setSelectedTime("");
+    console.log({allInfoTutors});
+    const handleTutorChange = (event) => {
+        const tutorId = event.target.value;
+        const selected = allInfoTutors.tutors.find((tutor) => tutor._id === tutorId);
+        setSelectedTutor(selected);
+        setSelectedDay("");
+        setSelectedTime("");
 
-    const preDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const toDay = new Date();
-    const toDayWoche = toDay.getDay();
+        const preDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        const toDay = new Date();
+        const toDayWoche = toDay.getDay();
 
-    const futureDates = selected.availability.map(days => {
-      const dayIndex = preDay.indexOf(days.day);
-      const difference = dayIndex - toDayWoche;
+        const futureDates = {};
+        selected.availability.forEach(days => {
+            const dayIndex = preDay.indexOf(days.day);
+            const difference = dayIndex - toDayWoche;
 
-      const futureDate = new Date(toDay);
-      if (difference < 0) {
-        futureDate.setDate(toDay.getDate() + difference + 7);
-      } else if (difference === 0) {
-        return `Today (${toDay.getDate()}.${toDay.getMonth() + 1})`;
-      } else {
-        futureDate.setDate(toDay.getDate() + difference);
-      }
+            const futureDate = new Date(toDay);
+            const [hours, minutes] = days.time.split(":")
+            futureDate.setHours(hours)
+            futureDate.setMinutes(minutes)
+            futureDate.setSeconds(0)
+            futureDate.setMilliseconds(0)
 
-      return `${preDay[dayIndex]} ${futureDate.getDate()}.${futureDate.getMonth() + 1}.${futureDate.getFullYear()} at  ${days.time} hours`;
-    });
-setTimeout(futureDates)
-    console.log({ futureDates });
+            if (difference < 0) {
+                futureDate.setDate(toDay.getDate() + difference + 7);
+            } else if (difference === 0) {
+                futureDates[toDay.toISOString()] = `Today (${toDay.getDate()}.${toDay.getMonth() + 1})`;
+                return;
+            } else {
+                futureDate.setDate(toDay.getDate() + difference);
+            }
 
-  };
-  const handleDayChange = (event) => {
-    const selectedDay = event.target.value;
-    setSelectedDay(selectedDay);
-  };
+            futureDates[futureDate.toISOString()] = `${preDay[dayIndex]} ${futureDate.getDate()}.${futureDate.getMonth() + 1}.${futureDate.getFullYear()} at ${days.time} hours`;
+        });
 
-  const handleTimeChange = (event) => {
-    const selectedTime = event.target.value;
-    setSelectedTime(selectedTime);
-  };
-  const handleThemeChange= (event) => {
-    const theme=event.target.value;
-    setTheme(theme);
-  }
-  const handleDateChange = (event) => {
-    const choosedate = event.target.value;
-   /* const parts = choosedate.split(' ');
-    const day = parts[1].slice(0, -1); // "16"
-    const month = parts[2].split('.')[0]; // "10"
-    const hours = parts[4].split(':')[0]; // "16"
-    const minutes = parts[4].split(':')[1]; // "30"
+        setTimeout(futureDates)
+        console.log({futureDates});
 
-    const date = new Date(2023, month - 1, day, hours, minutes); // Měsíce začínají od 0 v JavaScriptu
+    };
+    const handleDayChange = (event) => {
+        const selectedDay = event.target.value;
+        setSelectedDay(selectedDay);
+    };
 
-    const formattedDate = date.toISOString();*/
-    setDate(choosedate);
-  };
+    const handleTimeChange = (event) => {
+        const selectedTime = event.target.value;
+        setSelectedTime(selectedTime);
+    };
+    const handleThemeChange = (event) => {
+        const theme = event.target.value;
+        setTheme(theme);
+    }
+    const handleDateChange = (event) => {
+        const choosedate = event.target.value;
+        /* const parts = choosedate.match(/(\d{2})\.(\d{2})\.(\d{4}) at (\d{2}):(\d{2})/);
 
-const sendDatainTask = (event) => {
-  event.preventDefault();
-  console.log('klik')
-  const data={
-    tutor:selectedTutor.tutorName,
-    theme:theme,
-    user:selectedTutor._id,
-date:date
-  }
-  console.log(data)
-}
+         const year = parts[3];
+         const month = parts[2];
+         const day = parts[1];
+         const hour = parts[4];
+         const minute = parts[5];
 
-  return (
-    <div>
-      <header className="flex justify-between pl-10 pt-10">
-        <h1 className="text-3xl font-semibold text-slate-800">
-          Start today!
-        </h1>
-      </header>
-      <p className="text-slate-700 text-xs pl-10">
-        Reserve a class with our expert tutor and take one step closer to your academic goals.
-      </p>
-      <div className="p-10">
-        <Card>
-          <form>
-            <Label htmlFor="theme">Theme</Label>
-            <Input
-              type="text"
-              name="theme"
-              placeholder="Enter the theme"
-              value={theme}
-              onChange={handleThemeChange}
-            />
-            <Label htmlFor="tutorSelect">Choose a Tutor</Label>
-            <select
-              name="tutorSelect"
-              className="block w-full bg-gray-200 text-black mt-2 mb-2 px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-300"
-              onChange={handleTutorChange}
-            >
-              <option value="">Select tutor</option>
-              {allInfoTutors && allInfoTutors.tutors ? (
-                allInfoTutors.tutors.map((tutor) => (
-                  <option key={tutor._id} value={tutor._id}>
-                    {tutor.tutorName}
+         const localDate = new Date(Date.UTC(year, month - 1, day, hour, minute));
+         const formattedDate = localDate.toISOString();*/
+        setDate(choosedate);
+    };
 
-                  </option>
-                ))
-              ) : (
-                <option value="" disabled>
-                  Loading...
-                </option>
-              )}
-            </select>
+    const sendDatainTask = (event) => {
+        event.preventDefault();
+        console.log('klik')
+        const data = {
+            tutor: selectedTutor.tutorName,
+            theme: theme,
+            date: date
+        }
+        console.log(data)
+    }
 
-            {selectedTutor && (
-              <div className="mt-4">
-                <Label htmlFor="daySelect">Choose a Day</Label>
-                <select
-                  name="daySelect"
-                  onChange={handleDateChange}
-                  className="block w-full bg-gray-200 text-black mt-2 mb-2 px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-300"
-                 /* onChange={handleDayChange}*/
-                >
+    return (
+        <div>
+            <header className="flex justify-between pl-10 pt-10">
+                <h1 className="text-3xl font-semibold text-slate-800">
+                    Start today!
+                </h1>
+            </header>
+            <p className="text-slate-700 text-xs pl-10">
+                Reserve a class with our expert tutor and take one step closer to your academic goals.
+            </p>
+            <div className="p-10">
+                <Card>
+                    <form>
+                        <Label htmlFor="theme">Theme</Label>
+                        <Input
+                            type="text"
+                            name="theme"
+                            placeholder="Enter the theme"
+                            value={theme}
+                            onChange={handleThemeChange}
+                        />
+                        <Label htmlFor="tutorSelect">Choose a Tutor</Label>
+                        <select
+                            name="tutorSelect"
+                            className="block w-full bg-gray-200 text-black mt-2 mb-2 px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+                            onChange={handleTutorChange}
+                        >
+                            <option value="">Select tutor</option>
+                            {allInfoTutors && allInfoTutors.tutors ? (
+                                allInfoTutors.tutors.map((tutor) => (
+                                    <option key={tutor._id} value={tutor._id}>
+                                        {tutor.tutorName}
 
-                  <option value="">Select day</option>
-                  {timeout.map((book) =>  (
-                    <option key={book} value={book}>
-                      {book}
-                      {console.log({book}) }
-                    </option>
-                  ))}
-                </select>
-{/*
+                                    </option>
+                                ))
+                            ) : (
+                                <option value="" disabled>
+                                    Loading...
+                                </option>
+                            )}
+                        </select>
+
+                        {selectedTutor && (
+                            <div className="mt-4">
+                                <Label htmlFor="daySelect">Choose a Day</Label>
+                                <select
+                                    name="daySelect"
+                                    onChange={handleDateChange}
+                                    className="block w-full bg-gray-200 text-black mt-2 mb-2 px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+                                    /* onChange={handleDayChange}*/
+                                >
+
+                                    <option value="">Select day</option>
+                                    {Object.entries(timeout).map(([key, book]) => (
+                                        <option key={key} value={key}>
+                                            {book}
+                                        </option>
+                                    ))}
+                                </select>
+                                {/*
 
                {selectedDay && (
                   <div className="mt-4">
@@ -173,9 +180,9 @@ date:date
                     </select>
                   </div>
                )}*/}
-              </div>
-            )}
-{/*
+                            </div>
+                        )}
+                        {/*
             {selectedDay && selectedTime && (
               <div className="mt-4">
                 <h3>Detalles de la reserva:</h3>
@@ -186,10 +193,10 @@ date:date
               </div>
             )}*/}
 
-            <Button type="submit" onClick={sendDatainTask}>Confirm</Button>
-          </form>
-        </Card>
-      </div>
-    </div>
-  );
+                        <Button type="submit" onClick={sendDatainTask}>Confirm</Button>
+                    </form>
+                </Card>
+            </div>
+        </div>
+    );
 }
